12549eb51d410c3a11d0bc30d83ed0f7197390cf
Jeffrey Drakos <captainjeff@Jeffreys-MacBook-Pro.local>
2020-10-09 12:08:22 -0400

    merging index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,83 +1,92 @@
-declare module 'react-native-callkeep' {
-  export type Events =
-    'didReceiveStartCallAction' |
-    'answerCall' |
-    'endCall' |
-    'didActivateAudioSession' |
-    'didDeactivateAudioSession' |
-    'didDisplayIncomingCall' |
-    'didToggleHoldCallAction' |
-    'didPerformDTMFAction' |
-    'didResetProvider' |
-    'checkReachability' |
-    'didPerformSetMutedCallAction' |
-    'didLoadWithEvents';
+export type Events =
+  'didReceiveStartCallAction' |
+  'answerCall' |
+  'endCall' |
+  'didActivateAudioSession' |
+  'didDeactivateAudioSession' |
+  'didDisplayIncomingCall' |
+  'didToggleHoldCallAction' |
+  'didPerformDTMFAction' |
+  'didResetProvider' |
+  'checkReachability' |
+  'didPerformSetMutedCallAction';
 
-  type HandleType = 'generic' | 'number' | 'email';
+type HandleType = 'generic' | 'number' | 'email';
 
-  interface IOptions {
-    ios: {
-      appName: string,
-      imageName?: string,
-      supportsVideo?: boolean,
-      maximumCallGroups?: string,
-      maximumCallsPerCallGroup?: string,
-      ringtoneSound?: string,
-    },
-    android: {
-      alertTitle: string,
-      alertDescription: string,
-      cancelButton: string,
-      okButton: string,
-      imageName?: string,
-      additionalPermissions: string[],
-    },
+interface IOptions {
+  ios: {
+    appName: string,
+    imageName?: string,
+    supportsVideo?: boolean,
+    maximumCallGroups?: string,
+    maximumCallsPerCallGroup?: string,
+    ringtoneSound?: string,
+  },
+  android: {
+    alertTitle: string,
+    alertDescription: string,
+    cancelButton: string,
+    okButton: string,
+    imageName?: string,
+    additionalPermissions: string[],
+  },
+}
+
+export type DidReceiveStartCallActionPayload = { handle: string };
+export type AnswerCallPayload = { callUUID: string };
+export type EndCallPayload = AnswerCallPayload;
+export type DidDisplayIncomingCallPayload = string | undefined;
+export type DidPerformSetMutedCallActionPayload = boolean;
+
+export default class RNCallKeep {
+  static addEventListener(type: Events, handler: (args: any) => void) {
+
   }
 
-  export type DidReceiveStartCallActionPayload = { handle: string };
-  export type AnswerCallPayload = { callUUID: string };
-  export type EndCallPayload = AnswerCallPayload;
-  export type DidDisplayIncomingCallPayload = string | undefined;
-  export type DidPerformSetMutedCallActionPayload = boolean;
+  static removeEventListener(type: Events, handler: (args: any) => void) {
 
-  export default class RNCallKeep {
-    static addEventListener(type: Events, handler: (args: any) => void): void
+  }
 
-    static removeEventListener(type: Events): void
+  static async setup(options: IOptions): Promise<void> {
 
-    static setup(options: IOptions): Promise<void>
+  }
 
-    static hasDefaultPhoneAccount(): boolean
+  static hasDefaultPhoneAccount(): boolean {
 
-    static answerIncomingCall(uuid: string): void
+  }
 
-    static registerPhoneAccount(): void
+  static answerIncomingCall(uuid: string) {
 
-    static registerAndroidEvents(): void
+  }
 
-    static displayIncomingCall(
-      uuid: string,
-      handle: string,
-      localizedCallerName?: string,
-      handleType?: HandleType,
-      hasVideo?: boolean,
-    ): void
+  static displayIncomingCall(
+    uuid: string,
+    handle: string,
+    localizedCallerName?: string,
+    handleType?: HandleType,
+    hasVideo?: boolean,
+  ) {
 
-    static startCall(
-      uuid: string,
-      handle: string,
-      contactIdentifier?: string,
-      handleType?: HandleType,
-      hasVideo?: boolean,
-    ): void
+  }
 
-    static updateDisplay(
-      uuid: string,
-      displayName: string,
-      handle: string,
-    ): void
+  static startCall(
+    uuid: string,
+    handle: string,
+    contactIdentifier?: string,
+    handleType?: HandleType,
+    hasVideo?: boolean,
+  ) {
 
-    /**
+  }
+  static updateDisplay(
+    uuid: string,
+    displayName: string,
+    handle: string,
+  ) {
+
+  }
+
+  /**
      * @description reportConnectedOutgoingCallWithUUID method is available only on iOS.
      */
     static reportConnectedOutgoingCallWithUUID(uuid: string): void
